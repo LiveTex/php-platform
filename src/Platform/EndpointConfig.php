0805287e0e6bcebe6296939cf864fd91a70e4753
oleg <oleg@tebryaev.com>
2014-10-15 11:10:59 +0400

    ...

---
+++ src/Platform/EndpointConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Tebryaev Oleg <oleg@tebryaev.com>
+ */
+
+namespace Platform;
+
+class EndpointConfig
+{
+    public $namespace = 'Default';
+    public $service = 'Default';
+}
